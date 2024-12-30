ee2edb99de8367bde049cf5dc3b7e771024c49bd
javadi <javadi89s@gmail.com>
2024-12-30 17:06:32 +0330

    update-project

--- src/home/Inspiration.js
+++ src/home/Inspiration.js
@@ -7,13 +7,13 @@ const Inspiration = () => {
     const images = [inspire0, inspire1, inspire2, inspire3];
 
     return (
-        <div className="bg-light py-10">
+        <div className="bg-light py-20">
             <div className="flex flex-col lg:flex-row justify-center items-center lg:items-start px-5">
                 <div className="lg:text-left p-5 lg:w-[40%]">
                     <h1 className="text-5xl text-gray-700 font-bold lg:text-left">
                         50+ Beautiful rooms inspiration
                     </h1>
-                    <p className="text-lg font-semibold text-gray-500 my-6 lg:text-left">
+                    <p className="text-lg font-semibold text-muted my-6 lg:text-left">
                         Our designer already made a lot of beautiful room prototypes to inspire you.
                     </p>
                     <button className="bg-yellow-600 hover:bg-yellow-700 text-white font-bold text-lg px-8 py-3">
@@ -21,14 +21,14 @@ const Inspiration = () => {
                     </button>
                 </div>
 
-                <div className="overflow-x-scroll no-scrollbar w-full lg:w-[60%]">
+                <div className="overflow-x-scroll scroll-smooth w-full lg:w-[60%]">
                     <div className="flex space-x-6">
                         {images.map((img, index) => (
                             <img
                                 key={index}
                                 src={img}
                                 alt={`inspiration-${index}`}
-                                className="min-w-[400px] lg:min-w-[600px] h-[450px] object-cover shadow-lg"
+                                className="min-w-[600px] lg:min-w-[400px] h-[450px] object-cover shadow-lg"
                             />
                         ))}
                     </div>
