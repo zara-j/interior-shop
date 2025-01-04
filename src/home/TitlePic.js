58acb3f1e281357497e8f9dd7a6f1e6af03510dc
javadi <javadi89s@gmail.com>
2025-01-04 12:18:42 +0330

    responsive-titlePic

--- src/home/TitlePic.js
+++ src/home/TitlePic.js
@@ -3,20 +3,19 @@ import titleBg01 from '../assets/bg03.jpg';
 const TitlePic = () => {
   return (
     <div className="relative flex items-center justify-center lg:justify-end font-notoSans text-left">
-      <img className="h-screen w-screen object-cover" alt="Title Background" src={titleBg01} />
+      <img className="h-[50rem] w-screen object-cover" alt="Title Background" src={titleBg01} />
       
-      <div className="absolute bg-rose-950 rounded shadow-lg p-5 xl:mr-16 sm:p-8 md:p-10 lg:p-12"
-        style={{ maxWidth: '90%', width: '100%', maxWidth: '700px' }}>
-        <div className="container text-left">
+      <div className="absolute bg-rose-950 rounded-lg lg:w-[42rem] p-5 sm:p-8 sm:mr-32 md:p-10 lg:p-12 lg:right-[300px]">
+        <div className="container text-left p-4">
           <p className="text-lg sm:text-xl text-light">New Arrival</p>
-          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-amber-500 font-bold mb-4">
+          <h1 className="text-4xl sm:text-4xl md:text-6xl lg:text-6xl text-amber-500 font-bold mb-4">
             Discover Our New Collection
           </h1>
           <p className="text-light text-base sm:text-lg md:text-xl mb-6 sm:mb-8">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus nec ullamcorper mattis.
           </p>
-          <button className="bg-amber-500 text-light py-4 px-6 lg:px-16 sm:py-4 mt-3 mb-0 sm:w-auto"
-            style={{ paddingLeft: '3rem', paddingRight: '3rem' }}>
+          <button className="bg-amber-500 text-light font-bold py-4 px-6 lg:px-16 sm:py-4 mt-3 mb-0 sm:w-auto"
+            style={{ paddingLeft: '3.5rem', paddingRight: '3.5rem' }}>
             BUY NOW
           </button>
         </div>
