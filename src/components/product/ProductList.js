6bb366b527522af4bf273c162015374c0ec59470
javadi <javadi89s@gmail.com>
2025-01-11 12:18:48 +0330

    final pagination

--- src/components/product/ProductList.js
+++ src/components/product/ProductList.js
@@ -1,11 +1,16 @@
 import { useState } from 'react';
 import ProductCard from './ProductCard';
-import { products } from '../../data/products';
-const ProductList = ({ limit }) => {
-    const [showAll, setShowAll] = useState(false)
+import { products as defaultProducts } from '../../data/products'; // Default source for products
 
+const ProductList = ({ limit, currentItems, products }) => {
+    const [showAll, setShowAll] = useState(false);
 
-    const visibleProducts = showAll || !limit ? products : products.slice(0, limit)
+    // Determine the source of the product list
+    const finalProducts = currentItems || products || defaultProducts;
+
+    // Apply limit logic
+    const visibleProducts = showAll || !limit ? finalProducts : finalProducts.slice(0, limit);
+
     return (
         <div className="bg-white flex flex-col items-center py-10">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mx-5">
@@ -21,8 +26,10 @@ const ProductList = ({ limit }) => {
                 ))}
             </div>
             {limit && (
-                <button onClick={() => setShowAll(!showAll)}
-                    className="text-lg font-bold border-2 border-yellow-600 text-yellow-600 px-16 py-3 mt-4">
+                <button
+                    onClick={() => setShowAll(!showAll)}
+                    className="text-lg font-bold border-2 border-yellow-600 text-yellow-600 px-16 py-3 mt-4"
+                >
                     {showAll ? 'Show Less' : 'Show More'}
                 </button>
             )}
